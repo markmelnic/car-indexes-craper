3554eba56198e285093ebf76deedc18390bd4b8e
Mark Melnic <markhappy200@gmail.com>
2020-09-03 20:27:27 +0200

    Renamed main to test

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import anibis_ch
 
 
 if __name__ == '__main__':
-    
+
     try:
         #autoscout24_ch.scrape_makes()
         #autoscout24_ch.scrape_models()
@@ -21,4 +21,4 @@ if __name__ == '__main__':
         exit(0)
     except json.decoder.JSONDecodeError:
         os.remove('makes.json')
-        subprocess.run('python main.py')
+        subprocess.run('python test.py')
